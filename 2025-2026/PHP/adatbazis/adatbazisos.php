025c1e14fb28ec895403c23cabc85fa30ca350db
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-11-14 10:35:03 +0100

    fghgfhfg

--- 2025-2026/PHP/adatbazis/adatbazisos.php
+++ 2025-2026/PHP/adatbazis/adatbazisos.php
@@ -2,21 +2,25 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "php";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Create database
-$sql = "CREATE DATABASE php";
-if ($conn->query($sql) === true) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+$conn->query($sql);
 
+/*$query = "INSERT INTO `adatok` (`adat1`, `adat2`, `adat3`, `adat4`) VALUES ('12', 'qweerwq', '2025-11-28', '1212')";
+ $conn->query($query);*/
+
+$query = "SELECT adat1,adat4 from adatok";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo $row["adat1"] . " " . $row["adat4"] . "<br>";
+    }
+}
 $conn->close();
 ?>
