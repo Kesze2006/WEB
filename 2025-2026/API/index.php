c2ce532f2e3bda1b1e607fc5601b2bf18a531e66
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-11-24 13:18:46 +0100

    dfsgdf

--- 2025-2026/API/index.php
+++ 2025-2026/API/index.php
@@ -1,3 +1,27 @@
 <?php
-phpinfo(32);
-?>
+include_once "../PHP/fugvenyek.php";
+if (isset($_GET["path"])) {
+    $apiParts = explode("/", $_GET["path"]);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "todolist";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($apiParts[0] == "todo") {
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $query = "SELECT id, szoveg, datum, vege FROM todo";
+            $result = $conn->query($query);
+            $jsonTomb = [];
+            while ($row = $result->fetch_assoc()) {
+                $jsonTomb[] = $row;
+            }
+            $conn->close();
+        }
+        $json = json_encode($jsonTomb);
+        d($json);
+    }
+} else {
+     ?>
+<h3>API help</h3>
+<?php
+} ?>
