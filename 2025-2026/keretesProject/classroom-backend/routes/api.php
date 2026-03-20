d92d8984377329e61e7e52776b34facdd1cd232e
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2026-03-20 08:45:46 +0100

    dfsds

--- 2025-2026/keretesProject/classroom-backend/routes/api.php
+++ 2025-2026/keretesProject/classroom-backend/routes/api.php
@@ -4,9 +4,16 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
 
 Route::post("/register", [AuthController::class, "register"]);
+
+Route::get("/email", function () {
+    Mail::raw("Ez egy teszt email", function ($message) {
+        $message->to("keszericze.akso.21@ady-nagyatad.hu")->subject("Teszt");
+    });
+});
